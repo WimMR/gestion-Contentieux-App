5f28e39c4552660a4bf23d5c9c690c00719b13f8
WimMR <wim.ricci@gmail.com>
2023-06-23 11:29:45 +0200

    merge

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,19 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TacheService } from './services/tache.service';
+import { CaseComponent } from './pages/case/case.component';
+import { DocumentComponent } from './pages/document/document.component';
+import { FormsModule } from '@angular/forms';
+import { CaseService } from './services/case.service';
+import { HttpClientModule } from '@angular/common/http';
+import { DocumentService } from './services/document.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CaseComponent,
+    DocumentComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    AppRoutingModule,
+    HttpClientModule,
   ],
-  providers: [
-    TacheService
-  ],
+  providers: [CaseService, DocumentService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
